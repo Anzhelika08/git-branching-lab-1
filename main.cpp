925b849848b207c6727ba9a8c3a3d2daf796513d
Anzhelika08 <ustelemovaanzelika0@gmail.com>
2025-12-10 18:33:34 +0500

    Implement printlines

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include<fstream>
 
 using namespace std;
 vector<string> readLines(const string& path);
@@ -28,9 +29,10 @@ vector<string> readLines(const string&)
     return {};
 }
 
-void printLines(const vector<string>&) 
-{
-
+void printLines(const vector<string>& lines) {
+    for (const auto& s : lines) {
+        cout << s << '\n';
+    }
 }
 
 void writeLines(const vector<string>&, const string&) 
